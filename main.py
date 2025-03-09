8e5022a6eec3b2de9623ee4612ff68bf2b40ae6e
dev8psST <mailfreesw@protonmail.com>
2025-03-09 16:29:04 -0400

    next

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ def test_add():
     assert add(5,6) == 11
     assert add(34,6) == 40
     assert add(3.8,9)  == 12.8
-    assert add("Hello","pytest")
+    assert add("Hello","pytest") == "Hello pytest"
