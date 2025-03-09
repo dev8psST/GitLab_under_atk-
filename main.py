d910f8a975a547b1b9659dc9647c90d1dfc79091
dev8psST <mailfreesw@protonmail.com>
2025-03-09 16:31:45 -0400

    edit main.py mk three func

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pytest
 from add import add
 
-def test_add():
+def test_add_1():
     assert add(5,6) == 11
-    assert add(34,6) == 40
+    
     assert add(3.8,9)  == 12.8
+    
+
+def test_add_2():
+    assert add(34,6) == 40
+
+def test_add_3():
     assert add("Hello","pytest") == "Hello pytest"
