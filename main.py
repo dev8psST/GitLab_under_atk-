ac97ed724ddb5782bd2743862008047426be7f76
dev8psST <mailfreesw@protonmail.com>
2025-03-09 16:26:20 -0400

    add add.py & rewirte main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+import pytest
+from add import add
+
+def test_add():
+    assert add(5,6) == 11
+    assert add(34,6) == 40
+    assert add(3.8,9)  == 12.8
+    assert add("Hello","pytest")
